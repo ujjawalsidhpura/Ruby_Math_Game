4cb222a01f7a0e46d6770859984d671ec33c7bee
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-11-08 10:48:35 +0000

    game created and working fine

---
+++ player.rb
@@ -0,0 +1,16 @@
+class Player
+@@num_of_players = 0
+
+attr_accessor :life
+attr_reader :player_num
+
+def initialize
+ @@num_of_players += 1
+ @life = 3
+ @player_num = "Player #{@@num_of_players}"
+end
+
+end
+
+
+
